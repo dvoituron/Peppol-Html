9b350f7ac8f5b44f0ac336a748f1dbeab0f49e4b
Denis Voituron <dvoituron@outlook.com>
2026-01-15 20:02:39 +0100

    Add a peppolLoader

---
+++ PeppolWasm/Controls/PeppolLoader.razor.cs
@@ -0,0 +1,17 @@
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace PeppolWasm.Controls;
+
+public partial class PeppolLoader
+{
+  int ProgressPercent = 0;
+  FluentInputFileEventArgs[] Files = [];
+
+  private async Task OnCompletedAsync(IEnumerable<FluentInputFileEventArgs> files)
+  {
+    var file = files.FirstOrDefault();
+    ProgressPercent = 0;
+
+    
+  }
+}
